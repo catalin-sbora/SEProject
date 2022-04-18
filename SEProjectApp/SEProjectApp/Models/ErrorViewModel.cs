e3c25f7cd7572faef7f1f614c5d1c3b9f274f1de
Catalin Sbora <catalin.sbora@softelligence.net>
2022-04-18 14:44:23 +0300

    create initial project

---
+++ SEProjectApp/SEProjectApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SEProjectApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
