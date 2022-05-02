2849f12814df9500f76548ad8c88e3a55df96c51
Catalin Sbora <catalin.sbora@softelligence.net>
2022-05-02 14:29:33 +0300

    add the repositories and organize data access repos

---
+++ SEProjectApp/SEProjectApp.DataAccess/StudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using SEProjectApp.Abstractions.Repository;
+using SEProjectApp.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEProjectApp.DataAccess
+{
+    class StudentRepository: BaseRepository<Student>, IStudentsRepository
+    {
+
+    }
+}
